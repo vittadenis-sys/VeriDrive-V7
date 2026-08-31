c2c50e8470abecb250fe1dbcf75869e656808a49
vittadenis-sys <vittadenis@gmail.com>
2026-08-31 23:03:03 +0200

    feat: add components/RegisterServiceWorker.tsx

---
+++ components/RegisterServiceWorker.tsx
@@ -0,0 +1,2 @@
+"use client"; import { useEffect } from "react";
+export function RegisterServiceWorker(){useEffect(()=>{if("serviceWorker" in navigator) navigator.serviceWorker.register("/sw.js").catch(()=>undefined);},[]);return null;}
