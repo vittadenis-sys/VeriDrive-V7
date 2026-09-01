3725a54b107e131d3e035d7a03de6c0b827e7f8e
vittadenis-sys <vittadenis@gmail.com>
2026-09-01 20:54:22 +0200

    chore: clean homepage service duplication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { CarFront, ShieldCheck, SearchCheck, ArrowRight } from "lucide-react";
+import { CarFront, SearchCheck, ArrowRight } from "lucide-react";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
