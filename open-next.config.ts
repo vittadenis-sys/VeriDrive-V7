2d29c468ac05c01689503961692850cc5b1ef103
vittadenis-sys <vittadenis@gmail.com>
2026-09-09 06:36:40 +0200

    fix: restore compatible OpenNext Cloudflare config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  enableNodejsCompat: true,
-});
+export default defineCloudflareConfig({});
